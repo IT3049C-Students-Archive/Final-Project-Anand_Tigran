055658876d7d37435fef33ce3469f871676bbfde
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2021-08-04 14:23:03 -0400

    added a basic phaser template

--- game.js
+++ game.js
@@ -1,1 +1,29 @@
+const config ={
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: 1000,
+  height:800,
+  scale: {
+    mode:Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: {
+    preload,
+    create,
+    update
+  },
+};
 
+const game = new Phaser.game(config);
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
