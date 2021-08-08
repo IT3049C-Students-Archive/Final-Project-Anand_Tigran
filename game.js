b6526e0aaf473b1a43280f1bbe6d12202c812ae6
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2021-08-08 14:01:20 -0400

    added animation and cursor key inputs

--- game.js
+++ game.js
@@ -20,7 +20,6 @@ const config ={
   }
 };
 var ledge
-var cursors
 const game = new Phaser.Game(config);
 
 function preload(){
@@ -62,7 +61,7 @@ function create(){
   // walking animation
   this.anims.create({
     key:'walk',
-    frames: this.anims.generateFramesNames('player',{
+    frames: this.anims.generateFrameNames('player',{
       prefix: 'robo_player_',
       start: 2,
       end: 3,
@@ -82,8 +81,8 @@ function create(){
     frames: [{ key:'player', frame:'robo_player_1'}],
     frameRate: 15,
   });
-// enable cursor key events
-cursors = this.input.keyboard.createCursorKeys();
+  // enable cursor key events
+  this.cursors = this.input.keyboard.createCursorKeys();
 
 
   // set collision
@@ -91,6 +90,40 @@ cursors = this.input.keyboard.createCursorKeys();
   this.physics.add.collider(this.player, ledge);
 }
 
-function update(){
+function update() {
+  // Control the player with left or right keys
+  if (this.cursors.left.isDown) {
+    this.player.setVelocityX(-200);
+    if (this.player.body.onFloor()) {
+      this.player.play('walk', true);
+    }
+  } else if (this.cursors.right.isDown) {
+    this.player.setVelocityX(200);
+    if (this.player.body.onFloor()) {
+      this.player.play('walk', true);
+    }
+  } else {
+    // If no keys are pressed, the player keeps still
+    this.player.setVelocityX(0);
+    // Only show the idle animation if the player is footed
+    // If this is not included, the player would look idle while jumping
+    if (this.player.body.onFloor()) {
+      this.player.play('idle', true);
+    }
+  }
 
+  // Player can jump while walking any direction by pressing the space bar
+  // or the 'UP' arrow
+  if ((this.cursors.space.isDown || this.cursors.up.isDown) && this.player.body.onFloor()) {
+    this.player.setVelocityY(-350);
+    this.player.play('jump', true);
+  }
+
+  // If the player is moving to the right, keep them facing forward
+  if (this.player.body.velocity.x > 0) {
+    this.player.setFlipX(false);
+  } else if (this.player.body.velocity.x < 0) {
+    // otherwise, make them face the other side
+    this.player.setFlipX(true);
+  }
 }
