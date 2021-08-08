0acf0a7816cfc01c00c009aa1e2f8efc356e11eb
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2021-08-08 12:58:00 -0400

    change of files

--- game.js
+++ game.js
@@ -19,7 +19,6 @@ const config ={
     },
   }
 };
-var platforms;
 
 const game = new Phaser.Game(config);
 
@@ -41,11 +40,14 @@ function create(){
   const backgroundImage = this.add.image(0,0,"background").setOrigin(0,0);
   backgroundImage.setScale(2.5, 1);
   // create map and ledges
-  platforms = this.physics.add.staticGroup();
-  platforms.create(400, 767, 'ground').setScale(2).refreshBody();
-  platforms.create(600, 400, 'ground');
-  platforms.create(50, 250, 'ground');
-  platforms.create(750, 220, 'ground');
+  const map = this.make.tilemap({ key:"map"});
+  const tileset = map.addTilesetImage('kenney_simple_platformer', 'tiles');
+  const platforms = map.createStaticLayer('Platforms', tileset,0,200);
+  //platforms = this.physics.add.staticGroup();
+  //platforms.create(400, 767, 'ground').setScale(2).refreshBody();
+  //platforms.create(600, 400, 'ground');
+  //platforms.create(50, 250, 'ground');
+  //platforms.create(750, 220, 'ground');
 
 
 }
