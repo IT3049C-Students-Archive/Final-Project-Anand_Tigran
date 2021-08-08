8636619585ec5f137dac60dde1e92cbf9ad2ac77
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2021-08-08 12:46:35 -0400

    redone some of the images

--- game.js
+++ game.js
@@ -19,17 +19,35 @@ const config ={
     },
   }
 };
+var platforms;
 
 const game = new Phaser.Game(config);
 
 function preload(){
   // loaded the background image
   this.load.image("background", "assets/background.png");
+  //loading the map
+  this.load.image("spike","assets/images/spike.png");
+  //load player
+  this.load.atlas("player","assets/images/kenney_player.png","assets/images/kenney_player_atlas.json");
+  //this.load.image('ground', 'assets/platform.png');
+  // load tiles
+  this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
+  // load map
+  this.load.tilemapTiledJSON('map', 'assets/tilemaps/level1.json');
 }
 
 function create(){
   const backgroundImage = this.add.image(0,0,"background").setOrigin(0,0);
   backgroundImage.setScale(2.5, 1);
+  // create map and ledges
+  platforms = this.physics.add.staticGroup();
+  platforms.create(400, 767, 'ground').setScale(2).refreshBody();
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+
+
 }
 
 function update(){
