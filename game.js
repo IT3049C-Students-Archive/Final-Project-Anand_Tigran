bcb8c1147a5074db206e47b3c0aa52cf5d43dd43
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2021-08-08 15:05:13 -0400

    fixed ledge placement

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 const config ={
   type: Phaser.AUTO,
   parent: 'game',
-  width: 1000,
-  height:800,
+  width: 800,
+  height:640,
   scale: {
     mode:Phaser.Scale.RESIZE,
     autoCenter: Phaser.Scale.CENTER_BOTH
